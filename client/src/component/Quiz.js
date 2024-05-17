81cf320485a8e2f374e44da38927d4e18a789e98
Jashan Bansal <jashanbansal7861@gmail.com>
2024-05-17 19:34:44 +0530

    quiz button created

--- client/src/component/Quiz.js
+++ client/src/component/Quiz.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Questions } from './Questions';
 
 export const Quiz = () => {
+  const [check, setChecked] = useState(undefined)
+  function onNext(){
+    console.log("next");
+  }
+
+  function onPrev(){
+    console.log("prev");
+  }
   return (
-    <div>quiz</div>
+    <div className='container'>
+        <h1 className='title text-light'>Quiz Application</h1>
+
+        {/* display questions */}
+        {/* <Questions onChecked={onChecked} /> */}
+         <Questions/>
+        <div className='grid'>
+            {/* { trace > 0 ? <button className='btn prev' onClick={onPrev}>Prev</button> : <div></div>} */}
+            <button className='btn prev' onClick={onPrev}>Prev</button>
+            <button className='btn next' onClick={onNext}>Next</button>
+        </div>
+    </div>
   )
 }
