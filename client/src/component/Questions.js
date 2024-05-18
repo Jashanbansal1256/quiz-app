3f1ce4f3fa3ea1d0949165b1af77cb03b890a592
Jashan Bansal <jashanbansal7861@gmail.com>
2024-05-18 08:50:45 +0530

    question added

--- client/src/component/Questions.js
+++ client/src/component/Questions.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const Questions = () => {
+  const [check, setChecked] = useState(undefined);
+
+  function onSelect() {
+    console.log("radio button clicked");
+  }
   return (
-    <div>Questions</div>
-  )
-}
+    <div className="questions">
+      <h2 className="text-light">Sample Question 1</h2>
+      <ul>
+        <li>
+          <input
+            type="radio"
+            value={false}
+            name="options"
+            id="q1-option"
+            onChange={()=>onSelect()}
+          />
+          <label className="text-primary" htmlFor="q1-option">
+            option
+          </label>
+          <div className="check checked"></div>
+        </li>
+      </ul>
+    </div>
+  );
+};
