daed59106f842623bbecdb0d13fbd6a2a525517d
Jashan Bansal <jashanbansal7861@gmail.com>
2024-05-19 16:26:03 +0530

    api completed

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -28,17 +28,43 @@ export async function insertQuestions(req,res){
 }
 
 export async function dropQuestions(req,res){
-    res.json("api delete request")
+    try {
+        await Questions.deleteMany();
+        res.json({ msg: "Questions Deleted Successfully...!"});
+   } catch (error) {
+        res.json({ error })
+   }
 }
 // get all result
 export async function getResult(req,res){
-    res.json(" result api get request")
+    try {
+        const r = await Results.find();
+        res.json(r)
+    } catch (error) {
+        res.json({ error })
+    }
 }
 // post all result
 export async function storeResult(req,res){
-    res.json(" result api post request")
+    try {
+        const { username, result, attempts, points, achived } = req.body;
+        if(!username && !result) throw new Error('Data Not Provided...!');
+
+        Results.create({ username, result, attempts, points, achived })
+        .then (function(err, data){
+               res.json({ msg : "Result Saved Successfully...!"})
+        })
+
+   } catch (error) {
+        res.json({error})
+   }
 }
 // delete all result
 export async function dropResult(req,res){
-    res.json(" result api delete request")
+    try {
+        await Results.deleteMany();
+        res.json({ msg : "Result Deleted Successfully...!"})
+    } catch (error) {
+        res.json({ error })
+    }
 }
