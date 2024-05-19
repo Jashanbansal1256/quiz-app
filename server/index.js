686e287bc068e64613423fdc9fafe7d402c73dc1
Jashan Bansal <jashanbansal7861@gmail.com>
2024-05-19 16:08:02 +0530

    router for end point

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan"
 import cors from "cors";
 import {config} from "dotenv"
+import router from "./router/route.js";
 
 const app=express()
 
@@ -12,6 +13,9 @@ app.use(express.json())
 config();
 
 const port=process.env.PORT || 8080
+
+
+app.use("/api", router)
 
 app.get("/",(req,res)=>{
     try{
