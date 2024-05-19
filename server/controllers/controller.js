09e5c0cf6e64a7b8e63034f96cbd9accd85ff976
Jashan Bansal <jashanbansal7861@gmail.com>
2024-05-19 16:20:16 +0530

    connection to mongodb and data added using schema and model

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,28 +1,44 @@
-export  async function getQuestions(req,res){
-    res.json("questions api get request")
- }
- 
- 
- // insert all questions
- 
- export async function insertQuestions(req,res){
-     res.json("api post request")
- }
- 
- export async function dropQuestions(req,res){
-     res.json("api delete request")
- }
- 
- // get all result
- 
- export async function getResult(req,res){
-     res.json(" result api get request")
- }
- // post all result
- export async function storeResult(req,res){
-     res.json(" result api post request")
- }
- // delete all result
- export async function dropResult(req,res){
-     res.json(" result api delete request")
- }
+import Questions from "../models/questionSchema.js"
+import Results from "../models/resultSchema.js"
+import questions,{answers} from "../database/data.js"
+
+
+export async function getQuestions(req,res){
+   try{
+     const q =await Questions.find();
+     res.json(q)
+   }catch(error){
+    res.json({error})
+   }
+}
+
+
+// insert all questions
+
+export async function insertQuestions(req,res){
+    try {
+        Questions.insertMany({ questions, answers})
+            .then(function(err, data){
+
+            res.json({ msg: "Data Saved Successfully...!"})
+        })
+    }catch (error) {
+        res.json({ error })
+    }
+}
+
+export async function dropQuestions(req,res){
+    res.json("api delete request")
+}
+// get all result
+export async function getResult(req,res){
+    res.json(" result api get request")
+}
+// post all result
+export async function storeResult(req,res){
+    res.json(" result api post request")
+}
+// delete all result
+export async function dropResult(req,res){
+    res.json(" result api delete request")
+}
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import morgan from "morgan"
 import cors from "cors";
 import {config} from "dotenv"
 import router from "./router/route.js";
+import connect from "./database/conn.js";
 
 const app=express()
 
@@ -12,20 +13,32 @@ app.use(cors());
 app.use(express.json())
 config();
 
-const port=process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
+app.use("/api", router);
 
-app.use("/api", router)
+app.get("/", (req, res) => {
+  try {
+    res.json("Get Request");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.get("/",(req,res)=>{
-    try{
-  res.json("Get Request")
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.log("Cannot connect to the server");
     }
-    catch(error){
-        console.log(error)
-    }
-})
+  })
+  .catch((error) => {
+    console.log("Invalid Database Connection");
+  });
 
-app.listen(port,()=>{
-    console.log(`server connected to http://localhost:${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`server connected to http://localhost:${port}`)
+// })
